d0cbe4566ae0ee334e1c9a599e6da418545b2e22
nikola <nikolaristic03@gmail.com>
2022-04-16 20:57:01 +0200

    enctype

--- interface.js
+++ interface.js
@@ -4,10 +4,10 @@ function generateInterface(listFile) {
     $("#interfaceBtn").css("display", "none")
 
     s = `
-    <form id="box" action="http://proteinformatics.uni-leipzig.de/mutation_explorer/submit" method="post">
+    <form id="box" action="http://proteinformatics.uni-leipzig.de/mutation_explorer/submit" method="post" enctype="multipart/form-data">
         Alignment <br>
         
-        <select id='fileDropdown' onchange='changeSeqSelection()'></select>
+        <select id='fileDropdownConv' onchange='changeSeqSelectionConv()'></select>
         <br>
         <br>
         <br>
@@ -19,11 +19,6 @@ function generateInterface(listFile) {
 
         <input id="pdbConv" name="pdb_conv" type="text" placeholder="pdb id" onchange='clearInput("#fileConv")'> <br>
         <input id="fileConv" name="file_conv" type="file" accept=".pdb" onchange='clearInput("#pdbConv")'>
-        <br>
-        <br>
-        <br>
-
-        <br>
         <br>
         <br>
         <br>
@@ -40,13 +35,13 @@ function generateInterface(listFile) {
         .then(text => text.split("\n"))
         .then(files => {
             for (let i = 0; i < files.length; i++) {
-                $("#fileDropdown").append(`<option value='${files[i]}'>${files[i]}</option>`);
+                $("#fileDropdownConv").append(`<option value='${files[i]}'>${files[i]}</option>`);
             }
         });
 
 
 
-    changeSeqSelection()
+    changeSeqSelectionConv()
 
 }
 
@@ -83,19 +78,8 @@ function superInterface(listFile) {
         <select id="seqB"></select> <br>
         <br>
 
-        <input id="pdbB" name="pdb_b" type="text" placeholder="pdb id", onchange='checkSuperimpose("#fileB")'> <br>
-        <input id="fileB" name="file_b" type="file" accept=".pdb" onchange='checkSuperimpose("#pdbB")'>
-        <br>
-        <br>
-
-        <input id="superimpose" type="checkbox" disabled style="margin-left: 2vw"> <span id="superimposeLabel" style="opacity: 0.5">superimpose PDBs</span>
-        <br>
-        <br>
-        <br>
-
-        <input id="coloring" type="checkbox"> color by conservation
-        <br>
-        <br>
+        <input id="pdbB" name="pdb_b" type="text" placeholder="pdb id", onchange='clearInput("#fileB")'> <br>
+        <input id="fileB" name="file_b" type="file" accept=".pdb" onchange='clearInput("#pdbB")'>
         <br>
         <br>
 
@@ -111,9 +95,9 @@ function superInterface(listFile) {
         .then(text => text.split("\n"))
         .then(files => {
             for (let i = 0; i < files.length; i++) {
-                fsplit = '${files[i]}'.split("/");
-                filename = fsplit[fsplit.length - 1];
-                $("#fileDropdown").append(`<option value='${filename}'>${files[i]}</option>`);
+                //fsplit = `${files[i]}`.split("/");
+                //filename = fsplit[fsplit.length - 1];    TODO: remove path
+                $("#fileDropdown").append(`<option value='${files[i]}'>${files[i]}</option>`);
             }
         });
 
@@ -129,17 +113,6 @@ function clearInput(el) {
     $(el).val("");
 } 
 
-function checkSuperimpose(el) {
-    clearInput(el);
-    if($("#pdbB").val() == "" && $("#fileB").val() == "") {
-        $("#superimpose").prop("checked", false);
-        $("#superimpose").attr("disabled", true);
-        $("#superimposeLabel").css("opacity", "0.5");
-    } else {
-        $("#superimpose").attr("disabled", false);
-        $("#superimposeLabel").css("opacity", "1");
-    }
-}
 
 
 function changeSeqSelection() {
@@ -155,6 +128,22 @@ function changeSeqSelection() {
             for (let i = 0; i < arr.length; i++) {
                 $("#seqA").append(`<option value='${arr[i]}'>${arr[i]}</option>`);
                 $("#seqB").append(`<option value='${arr[i]}'>${arr[i]}</option>`);
+            }
+        });
+}
+
+
+function changeSeqSelectionConv() {
+    file = $("#fileDropdownConv").children("option:selected").val();
+    fetch(file)
+        .then(response => response.text())
+        //.then(text => console.log(text))
+        .then(text => text.split("\n"))
+        .then(lines => [lines[2].split(/[ ]+/)[0], lines[3].split(/[ ]+/)[0]])
+        .then(arr => {
+            $("#seq").children().remove();
+            for (let i = 0; i < arr.length; i++) {
+                $("#seq").append(`<option value='${arr[i]}'>${arr[i]}</option>`);
             }
         });
 }
