9f2926f17369d3d5d8721b36d1d89980474ae557
nikola <nikolaristic03@gmail.com>
2022-04-19 00:18:54 +0200

    required conv pdb

--- interface.js
+++ interface.js
@@ -59,7 +59,11 @@ function generateInterface(listFile) {
 }
 
 
-
+var $inputs = $('input[name=pdb_conv],input[name=file_conv]');
+$inputs.on('input', function () {
+    // Set the required property of the other input to false if this input is not empty.
+    $inputs.not(this).prop('required', !$(this).val().length);
+});
 
 
 
